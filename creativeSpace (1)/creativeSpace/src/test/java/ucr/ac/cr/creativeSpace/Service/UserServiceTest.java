b3231b0b261771895ad123f5f82e501fc30d8732
BrandonViquezCamacho <lingtnig0xand@gmail.com>
2026-06-17 12:16:22 -0600

    Completo las pruebas

--- creativeSpace (1)/creativeSpace/src/test/java/ucr/ac/cr/creativeSpace/Service/UserServiceTest.java
+++ creativeSpace (1)/creativeSpace/src/test/java/ucr/ac/cr/creativeSpace/Service/UserServiceTest.java
@@ -3,42 +3,89 @@ package ucr.ac.cr.creativeSpace.Service;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import ucr.ac.cr.creativeSpace.Model.DTO.UserDTO;
 import ucr.ac.cr.creativeSpace.Model.User;
 import ucr.ac.cr.creativeSpace.Repository.UserRepository;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+
+  // Permite que las anotaciones @Mock y @InjectMocks
+  // funcionen automáticamente durante las pruebas
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
-    @Mock
-    private UserRepository repository;
+    @Mock // crea simulacion
+    private UserRepository userRepository;
 
-    @InjectMocks
-    private UserService service;
+    @InjectMocks // crea una instancia e inyecta mock de repository
+    private UserService userService;
 
-    @Test
+    @Test // define una prueba unitaria
     void saveUserSuccessfully() {
 
         User user = new User();
-
+        user.setId(1);
         user.setName("Brandon");
         user.setEmail("brandon@gmail.com");
-        user.setPassword("1234");
+        user.setPassword("123478Ba");
+        user.setRol("ADMIN");
 
-        when(repository.save(any(User.class)))
+        // simular el repositorio y devuelve el usuario
+        when(userRepository.save(any(User.class)))
                 .thenReturn(user);
 
-        User result = repository.save(user);
+        UserDTO result = userService.saveUser(user);
+
+        assertNotNull(result); // ve que no sea null
+        assertEquals("Brandon", result.getName()); // verifica que lo registrado sea lo esperado
+        assertEquals("brandon@gmail.com", result.getEmail());
+
+        verify(userRepository, times(1))
+                .save(any(User.class)); // comprueba que save fue invocado una vez correctamente
+    }
+
+    @Test
+    void findUserByIdSuccessfully() {
+
+        User user = new User();
+        user.setId(1);
+        user.setName("Brandon");
+        user.setEmail("brandon@gmail.com");
+        user.setRol("ADMIN");
+
+        when(userRepository.findById(1))
+                .thenReturn(Optional.of(user));
+
+        UserDTO result = userService.findByIDUser(1);
 
         assertNotNull(result);
+        assertEquals(1, result.getId());
+        assertEquals("Brandon", result.getName());
 
-        verify(repository).save(any(User.class));
+        verify(userRepository, times(1))
+                .findById(1);
     }
+
+    @Test
+    void findUserByIdNotFound() {
+
+        when(userRepository.findById(99))
+                .thenReturn(Optional.empty());
+
+        UserDTO result = userService.findByIDUser(99);
+
+        assertNull(result); // lo contrario al positivo, se espera que sea null, así que lo admite
+
+        verify(userRepository, times(1))
+                .findById(99);
+    }
+
 }
